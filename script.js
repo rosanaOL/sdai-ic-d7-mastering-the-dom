c7afae1f430ee5065a35ce8b87dde82c14b9f09c
rosanaOL <ros.levesque@gmail.com>
2024-11-14 04:32:03 +0000

    final

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Write your JavaScipt code below this line. 
+const mainTitle = document.getElementById('main-title')
+mainTitle.textContent = 'Welcome to Our Cat Adoption Center'
+
+const description = document.getElementById('description')
+description.textContent = 'Find your purrfect companion!'
+
+const cat1Button = document.getElementById('cat-1-button')
+cat1Button.addEventListener('click', () => {
+    const cat1 = document.getElementById('cat-1')
+    alert('Congrats on adopting your new friend!')
+    cat1.remove()
+  })
+
+  const cat2Button = document.getElementById('cat-2-button')
+  cat2Button.addEventListener('click', () => {
+      const cat2 = document.getElementById('cat-2')
+      alert('Congrats on adopting your new friend!')
+      cat2.remove()
+    })
+  const cat3Button = document.getElementById('cat-3-button')
+  cat3Button.addEventListener('click', () => {
+      const cat3 = document.getElementById('cat-3')
+      alert('Congrats on adopting your new friend!')
+      cat3.remove()
+    })
